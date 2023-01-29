7bf553547914ed71e54663e960c446e6bc0a804c
ANDREA DATTILA s303339 <s303339@studenti.polito.it>
2023-01-29 22:10:41 +0100

    Trained TRPO and SAC for 100k

--- train.py
+++ train.py
@@ -5,7 +5,8 @@
 """
 import gym
 from env.custom_hopper import *
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
+from sb3_contrib import TRPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 import os
@@ -56,9 +57,84 @@ def main():
     print(evaluate_policy(model, target_env, n_eval_episodes=50, render=False))
 
     print("Target-Target environment results:")
-    # print(evaluate_policy(model_target, target_env, n_eval_episodes=50, render=False))
+    print(evaluate_policy(model_target, target_env, n_eval_episodes=50, render=False))
     
+
+
+
+
+
+    TRPO_path = os.path.join("training", "models", "TRPO_100k")  
+    TRPO_target_path = os.path.join("training", "models", "TRPO_TARGET_100k")  
+
+    print("--- TRAIN TRPO ON SOURCE ENVIRONMENT --- ")      
+    if os.path.exists("training/models/TRPO_100k.zip"):
+        print("Found source model!")
+        model = TRPO.load("training/models/TRPO_100k", env=env)
+    else:
+        print("source model file not found. training...")
+        model = TRPO("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=100000)
+        model.save(TRPO_path)
+    print("--- TRAIN TRPO ON TARGET ENVIRONMENT --- ")      
+    if os.path.exists("training/models/TRPO_TARGET_100k.zip"):
+        print("Found target model!")
+        model_target = TRPO.load("training/models/TRPO_TARGET_100k", env=env)
+    else:
+        print("target model file not found. training...")
+        model_target = TRPO("MlpPolicy", target_env, verbose=1)
+        model_target.learn(total_timesteps=100000)
+        model_target.save(TRPO_target_path)
+
+
+    print("Source-Source environment results:")
+    print(evaluate_policy(model, env, n_eval_episodes=50, render=False))
+    
+    print("Source-Target environment results:")
+    print(evaluate_policy(model, target_env, n_eval_episodes=50, render=False))
+
+    print("Target-Target environment results:")
+    print(evaluate_policy(model_target, target_env, n_eval_episodes=50, render=False))
+    
+
+
+
+
+
+    SAC_path = os.path.join("training", "models", "SAC_100k")  
+    SAC_target_path = os.path.join("training", "models", "SAC_TARGET_100k")  
+
+    print("--- TRAIN SAC ON SOURCE ENVIRONMENT --- ")      
+    if os.path.exists("training/models/SAC_100k.zip"):
+        print("Found source model!")
+        model = SAC.load("training/models/SAC_100k", env=env)
+    else:
+        print("source model file not found. training...")
+        model = SAC("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=100000)
+        model.save(SAC_path)
+    print("--- TRAIN SAC ON TARGET ENVIRONMENT --- ")      
+    if os.path.exists("training/models/SAC_TARGET_100k.zip"):
+        print("Found target model!")
+        model_target = SAC.load("training/models/SAC_TARGET_100k", env=env)
+    else:
+        print("target model file not found. training...")
+        model_target = SAC("MlpPolicy", target_env, verbose=1)
+        model_target.learn(total_timesteps=100000)
+        model_target.save(SAC_target_path)
+
+
+    print("Source-Source environment results:")
+    print(evaluate_policy(model, env, n_eval_episodes=50, render=False))
+    
+    print("Source-Target environment results:")
+    print(evaluate_policy(model, target_env, n_eval_episodes=50, render=False))
+
+    print("Target-Target environment results:")
+    print(evaluate_policy(model_target, target_env, n_eval_episodes=50, render=False))
+
     env.close()
+
 
 if __name__ == '__main__':
     main()
