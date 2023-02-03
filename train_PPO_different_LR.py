cc9adf479a9eb40a20aa9ded44bc51512263f1cf
ANDREA DATTILA s303339 <s303339@studenti.polito.it>
2023-02-03 18:06:03 +0100

    fix train_PPO_different_LR.py

--- train_PPO_different_LR.py
+++ train_PPO_different_LR.py
@@ -25,43 +25,43 @@ def main():
 
     # hyperparameter: learning rate
     learning_rates = [1e-3, 3e-4, 1e-4, 1e-5, 1e-6]
-    i=0 #change this value to change learning_rate
-    lr = learning_rates[i]
+    i=0
+        
+    for lr in learning_rates:    
+        PPO_path = os.path.join("training", "models", "PPO", f"LR{i}")  
+        PPO_target_path = os.path.join("training", "models", "PPO", f"TARGET_LR{i}")  
 
-    PPO_path = os.path.join("training", "models", "PPO", f"LR{i}")  
-    PPO_target_path = os.path.join("training", "models", "PPO", f"TARGET_LR{i}")  
-
-    print(f"--- TRAIN PPO ON SOURCE ENVIRONMENT (Learning Rate = {lr})--- ")      
-    if os.path.exists(f"training/models/PPO/LR{i}.zip"):
-        print("Found source model!")
-        model = PPO.load(f"training/models/PPO/LR{i}", env=env)
-    else:
-        print("source model file not found. training...")
-        model = PPO("MlpPolicy", env, verbose=1, learning_rate=lr)
-        model.learn(total_timesteps=100000)
-        model.save(PPO_path)
-    print(f"--- TRAIN PPO ON TARGET ENVIRONMENT (Learning Rate = {lr})--- ")      
-    if os.path.exists(f"training/models/PPO/TARGET_LR{i}.zip"):
-        print("Found target model!")
-        model_target = PPO.load(f"training/models/PPO/TARGET_LR{i}", env=env)
-    else:
-        print("target model file not found. training...")
-        model_target = PPO("MlpPolicy", target_env, verbose=1, learning_rate=lr)
-        model_target.learn(total_timesteps=100000)
-        model_target.save(PPO_target_path)
+        print(f"--- TRAIN PPO ON SOURCE ENVIRONMENT (Learning Rate = {lr})--- ")      
+        if os.path.exists(f"training/models/PPO/LR{i}.zip"):
+            print("Found source model!")
+            model = PPO.load(f"training/models/PPO/LR{i}", env=env)
+        else:
+            print("source model file not found. training...")
+            model = PPO("MlpPolicy", env, verbose=1, learning_rate=lr)
+            model.learn(total_timesteps=100000)
+            model.save(PPO_path)
+        print(f"--- TRAIN PPO ON TARGET ENVIRONMENT (Learning Rate = {lr})--- ")      
+        if os.path.exists(f"training/models/PPO/TARGET_LR{i}.zip"):
+            print("Found target model!")
+            model_target = PPO.load(f"training/models/PPO/TARGET_LR{i}", env=target_env)
+        else:
+            print("target model file not found. training...")
+            model_target = PPO("MlpPolicy", target_env, verbose=1, learning_rate=lr)
+            model_target.learn(total_timesteps=100000)
+            model_target.save(PPO_target_path)
 
 
-    print("Source-Source environment results:")
-    print(evaluate_policy(model, env, n_eval_episodes=50, render=False))
-    
-    print("Source-Target environment results:")
-    print(evaluate_policy(model, target_env, n_eval_episodes=50, render=False))
+        print("Source-Source environment results:")
+        print(evaluate_policy(model, env, n_eval_episodes=50, render=False))
+        
+        print("Source-Target environment results:")
+        print(evaluate_policy(model, target_env, n_eval_episodes=50, render=False))
 
-    print("Target-Target environment results:")
-    print(evaluate_policy(model_target, target_env, n_eval_episodes=50, render=False))
+        print("Target-Target environment results:")
+        print(evaluate_policy(model_target, target_env, n_eval_episodes=50, render=False))
+        i+=1
 
     env.close()
-
 
 if __name__ == '__main__':
     main()
